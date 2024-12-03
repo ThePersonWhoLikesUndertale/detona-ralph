f2f92200432fe9dbfb931eb72495199d42ad4bf3
TPWLU <sppaulisto972@gmail.com>
2024-12-03 17:13:21 -0300

    add countDown function and refactor

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -1,15 +1,30 @@
 const state = {
-    view: {
-        squares: document.querySelectorAll(".square"),
-        enemy: document.querySelector(".enemy"),
-        timeLeft: document.querySelector("time-left"),
-        score: document.querySelector("#score")
-    },
-    values: {
-        timerId: null,
-        gameVelocity: 1000,
-        hitPosition: 0,
-        result: 0
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+  },
+  values: {
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+  },
+  actions: {
+    timerId: setInterval(randomSquare, 1000),
+    countDownTimerId: setInterval(countDown, 1000),
+  },
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
     }
 }
 
@@ -22,10 +37,6 @@ function randomSquare() {
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
     state.values.hitPosition = randomSquare.id;
-}
-
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
 function addListenerHitBox() {
@@ -41,7 +52,6 @@ function addListenerHitBox() {
 }
 
 function initialize() {
-    moveEnemy();
     addListenerHitBox();
 }
 
